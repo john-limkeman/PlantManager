459e7fc8fe7a10e45c1358e78724c4a335c15eaf
John Limkeman <johnlimkeman@johns-mbp-3.lan>
2020-10-23 15:32:17 -0500

    first push

--- PlantManager/src/main/java/com/plantManager/Models/Plant.java
+++ PlantManager/src/main/java/com/plantManager/Models/Plant.java
@@ -2,4 +2,14 @@ package com.plantManager.Models;
 
 public class Plant {
 
+	public int id; //for DB
+	public String given_name; // given by user
+	public String common_name; //trefle data
+	public String scientific_name; //trefle data
+	public String genus; //trefle data
+	public String family; //trefle data
+	public String image_url; //trefle data
+	public boolean edible; //trefle data
+	public String linkToSelf; //link for API call to self data
+	
 }
