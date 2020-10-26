8eb1996250af6575cd5f50c0158b4a7cc9558466
John Limkeman <johnlimkeman@johns-mbp-3.lan>
2020-10-26 16:12:36 -0500

    stuck with the package.json

--- PlantManager/Vue/views/GardenView.vue
+++ PlantManager/Vue/views/GardenView.vue
@@ -1,13 +1,18 @@
 <template>
+  <div>
     <h1>YOUR GARDEN</h1>
+    <div id='gardenBox'>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+#gardenBox{
+    display: grid;
+    border: solid black 2px;
+}
 </style>
