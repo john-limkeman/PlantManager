b02bebcddf3fd4fd5086580def8bb5428822f274
john-limkeman <john.limkeman@gmail.com>
2020-12-07 23:59:01 -0600

    added sass

--- PlantManager/Vue/plant-manager/src/views/GardenView.vue
+++ PlantManager/Vue/plant-manager/src/views/GardenView.vue
@@ -2,6 +2,7 @@
   <div class='container'>
     <h1>MY GARDEN</h1>
     <div id="gardenBox">
+      <!-- generates plots, assigns IDs -->
       <div
         v-for="plot in plots"
         v-bind:key="plot.id"
@@ -12,6 +13,7 @@
         @dragover.prevent
         @dragenter.prevent
       >
+      <!-- for loop puts plants in plots -->
       <div v-for='plant in plantPlots(plot.id)' v-bind:key='plant.id' draggable @dragstart='startDrag($event, plant)'>
         <img v-bind:src='plant.img'/>
       </div>
@@ -20,6 +22,7 @@
     </div>
     <aside >
 
+<!-- HTML for panel -->
       <div v-if='PanelOpen' id='openPanel'>
        <button v-on:click='togglePanel()'>CLOSE</button>
       <PlantMenu/>
@@ -49,7 +52,7 @@ export default {
           id: 2,
           name: "potato",
           img:
-            "https://www.alimentarium.org/en/system/files/thumbnails/image/AL027-01_pomme_de_terre_0.jpg",
+            'plant-manager/src/Resources/potato_clip_art-removebg-preview.png',
           plot: 5,
         },
       ],
@@ -95,7 +98,7 @@ export default {
       return chosen;
     },
     toggleColor(plot) {
-      if (document.getElementById("plot" + plot.id).style.backgroundColor =='rgb(70, 34, 20)') { //CAN'T dig backwards?
+      if (document.getElementById("plot" + plot.id).style.backgroundColor =='rgb(70, 34, 20)') { 
         document.getElementById("plot" + plot.id).style.backgroundColor =
           "green";
       } else {
@@ -127,71 +130,86 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+
 :root {
-  --rowNum: 4;
-  --columnNum: 4;
+    --rowNum: 4;
+    --columnNum: 4;
+  }
+  #container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #gardenBox {
+    display: grid;
+    border: solid black 2px;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-areas:
+      "1 2 3 4 5 6 7 8 9 10"
+      "11 12 13 14 15 16 17 18 19 20"
+      "21 22 23 24 25 26 27 28 29 30"
+      "31 32 33 34 35 36 37 38 38 40"
+      "41 42 43 44 45 46 47 48 49 50"
+      "51 52 53 54 55 56 57 58 59 60"
+      "61 62 63 64 65 66 67 68 69 70"
+      "71 72 73 74 75 76 77 78 79 80"
+      "81 82 83 84 85 86 87 88 89 90"
+      "91 92 93 94 95 96 97 98 99 100";
+    grid-gap: 3px;
+    width: 1030px;
+    height: 1030px;
+
+  }
+  
+  
+  
+  img{
+    max-height: 50px;
+    max-width: 50px;
+  }
+  .plotBox {
+    background-color: green;
+    border: solid, black, 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    width: 100px;
+  }
+  
+  .plotBox:hover{
+    transition-duration: 1s;
+    transition-property: background-color;
+    background-color: lightgreen;
+  }
+  #closedPanel{
+       position: fixed;
+      right:0px;
+      top: 20%;
+      bottom: 20%;
+      width: 30px;
+      background-color: lightgoldenrodyellow;
+      
+  }
+
+$x: 1;
+@if $x == 1{
+  #closedPanel {
+    background-color: black;
+  }
 }
-#container{
-  display: flex;
-  align-items: flex-start;
-}
-#gardenBox {
-  display: grid;
-  border: solid black 2px;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-areas:
-    "1 2 3 4 5 6 7 8 9 10"
-    "11 12 13 14 15 16 17 18 19 20"
-    "21 22 23 24 25 26 27 28 29 30"
-    "31 32 33 34 35 36 37 38 38 40"
-    "41 42 43 44 45 46 47 48 49 50"
-    "51 52 53 54 55 56 57 58 59 60"
-    "61 62 63 64 65 66 67 68 69 70"
-    "71 72 73 74 75 76 77 78 79 80"
-    "81 82 83 84 85 86 87 88 89 90"
-    "91 92 93 94 95 96 97 98 99 100";
-  grid-gap: 3px;
-  width: 1030px;
-  height: 1030px;
-}
-img{
-  max-height: 50px;
-  max-width: 50px;
-}
-.plotBox {
-  background-color: green;
-  border: solid, black, 2px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100px;
-  width: 100px;
-}
-.plotBox:hover{
-  transition-duration: 1s;
-  transition-property: background-color;
-  background-color: lightgreen;
-}
-#closedPanel{
-     position: fixed;
-    right:0px;
-    top: 20%;
-    bottom: 20%;
-    width: 30px;
-    background-color: lightgoldenrodyellow;
-    
-}
-#openPanel{
-    position: fixed;
-    right:0px;
-    top: 20%;
-    bottom: 20%;
-    width: 200px;
-    background-color: lightgoldenrodyellow;
-}
-#closedPanel:hover{
-  cursor:pointer;
-  background-color:lightsalmon;
-}
+  #openPanel{
+      position: fixed;
+      right:0px;
+      top: 20%;
+      bottom: 20%;
+      width: 200px;
+      background-color: lightgoldenrodyellow;
+  }
+  #closedPanel:hover{
+    cursor:pointer;
+    background-color:lightsalmon;
+  }
+
 </style>
