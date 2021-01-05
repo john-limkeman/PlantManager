80cbc150a95fc4503f30ac6cf1dc943926d2a66b
john-limkeman <john.limkeman@gmail.com>
2021-01-05 15:10:45 -0600

    database work

--- PlantManager/Java/data/schema.sql
+++ PlantManager/Java/data/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS plants;
+DROP TABLE IF EXISTS gardens;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
@@ -18,6 +19,17 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+INSERT INTO users (user_id, username, password_hash, role) VALUES (1, 'user', 'pass', 'admin');
+
+CREATE TABLE gardens (
+        id serial NOT NULL,
+        user_id int NOT NULL,
+        CONSTRAINT pk_gardens_id PRIMARY KEY (id),
+        CONSTRAINT fk_gardens_user_id foreign key (user_id) references users (user_id)
+);
+
+INSERT INTO gardens (user_id) VALUES (1);
+
 CREATE TABLE plants (
         id serial NOT NULL,
         user_id int NOT NULL,
@@ -25,25 +37,16 @@ CREATE TABLE plants (
         name varchar(64) NOT NULL,
         water_rate int,
         sunlight_pref varchar (64),
-        date_planted date,
-        date_flowered date,
-        date_produced date,
+        --date_planted date,
+        --date_flowered date,
+        --date_produced date,
         plot int,
         CONSTRAINT PK_id PRIMARY KEY (id),
         CONSTRAINT fk_user_id foreign key (user_id) references users (user_id)
 );
 
-DECLARE num int := 1;
-CREATE TABLE gardens (
-        id serial NOT NULL,
-        user_id int NOT NULL,
-        WHILE num < 101
-        LOOP
-          plot + num boolean,
-          SET num = num + 1
-        END LOOP,
-        CONSTRAINT PK_id PRIMARY KEY (id),
-        CONSTRAINT fk_user_id foreign key (user_id) references users (user_id)
-);
+INSERT INTO plants (user_id, garden_id, name, water_rate, plot) VALUES (1, 1, 'Jeff', 2, 7);
+
+
 
 COMMIT TRANSACTION;
